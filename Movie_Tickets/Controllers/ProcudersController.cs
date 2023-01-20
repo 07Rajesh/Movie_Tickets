6fa334d0d4b85717a521f93fea70c63dc4c5b964
rajesh <rajeshpgdav3444@gmail.com>
2023-01-20 12:50:08 +0530

    Add project files.

---
+++ Movie_Tickets/Controllers/ProcudersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Movie_Tickets.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie_Tickets.Controllers
+{
+   
+    public class ProcudersController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProcudersController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allProducers = await _context.Producers.ToListAsync();
+
+            return View();
+        }
+    }
+}
